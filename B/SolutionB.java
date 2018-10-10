e464b352ab7e3b325dcc1e2d6125ca935da3d346
vivektheeyarath <vivektheeyarath@gmail.com>
2018-10-10 10:44:11 +0530

    Section B done

---
+++ B/SolutionB.java
@@ -0,0 +1,210 @@
+import java.util.Arrays;
+
+
+interface Animal {
+	
+}
+
+interface LandAnimal extends Animal{
+	
+	public String walk();
+	public String sing();
+	
+}
+
+interface FlyingAnimal extends LandAnimal {
+	
+	public String fly();
+		
+}
+
+interface SwimmingAnimal extends Animal{
+	
+	public String swim();
+		
+}
+
+class Birdy implements FlyingAnimal {
+
+	@Override
+	public String fly() {
+		return "I am flying";
+	}
+
+	@Override
+	public String walk() {
+		return "I am walking";
+	}
+
+	@Override
+	public String sing() {
+		return "I am singing";
+	}
+	
+}
+
+class Duck extends Birdy implements SwimmingAnimal {
+
+	@Override
+	public String swim() {
+		return "I am swimming";
+	}
+
+	@Override
+	public String sing() {
+		return "Quack, Quack";
+	}
+	
+}
+
+class Chicken extends Birdy {
+
+	private boolean isMale = false;
+	
+	public Chicken(boolean isMale) {
+		this.isMale = isMale;
+	}
+	
+	@Override
+	public String sing() {
+		return "Cluck, Cluck";
+	}
+
+	@Override
+	public String fly() {
+		return "Wings clipped";
+	}
+	
+}
+
+class Rooster implements LandAnimal {
+
+	private Chicken chicken = null;
+	
+	public Rooster() {
+		chicken = new Chicken(true);
+	}
+	
+	@Override
+	public String sing() {
+		return "Cock-a-doodle-do";
+	}
+
+	@Override
+	public String walk() {
+		return chicken.walk();
+	}
+
+}
+
+class Mammals implements LandAnimal {
+
+	@Override
+	public String walk() {
+		return "I am walking";
+	}
+
+	@Override
+	public String sing() {
+		return "Brrr";
+	}
+	
+	
+}
+
+class Cat extends Mammals {
+
+	@Override
+	public String sing() {
+		return "Meow";
+	}
+	
+}
+
+class Dog extends Mammals {
+
+	@Override
+	public String sing() {
+		return "Woof, woof";
+	}
+	
+}
+
+class Parrot extends Birdy {
+	
+	private LandAnimal livingWithOrNear = null;
+	
+	public Parrot(LandAnimal livingWithOrNear) {
+		this.livingWithOrNear = livingWithOrNear;
+	}
+	
+	@Override
+	public String sing() {
+		return this.livingWithOrNear.sing();
+	}
+}
+
+enum SIZE {LARGE,SMALL};
+enum COLOR {GREY, ORANGE};
+abstract class Fish implements SwimmingAnimal {
+
+	private SIZE size;
+	private COLOR color;
+	
+	protected Fish(SIZE size, COLOR color) {
+		this.size = size;
+		this.color = color;
+	}
+	
+	@Override
+	public String swim() {
+		return "Fish Swim";
+	}
+	
+	public SIZE getSize() {
+		return this.size;
+	}
+	
+	public COLOR getColor() {
+		return this.color;
+	}
+}
+
+class Shark extends Fish {
+	
+	public Shark() {
+		super(SIZE.LARGE,COLOR.GREY);
+	}
+	
+	public String eat() {
+		return "Other Fish";
+	}
+	
+}
+
+class ClownFish extends Fish {
+	
+	public ClownFish() {
+		super(SIZE.SMALL,COLOR.ORANGE);
+	}
+			
+	public String joke() {
+		return "joke";
+	}
+}
+
+class Dolphin implements SwimmingAnimal {
+
+	@Override
+	public String swim() {
+		return "swim";
+	}
+	
+}
+
+public class SolutionB {
+	
+	public static void main(String...args) {
+	}
+	
+}
---
+++ B/SolutionBTest.java
@@ -0,0 +1,95 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SolutionBTest {
+
+	@Test
+	public void testDuckSwim() {
+		SwimmingAnimal duck = new Duck();
+		
+		Assert.assertEquals("I am swimming", duck.swim());
+	}
+	
+	@Test
+	public void testDuckSing() {
+		LandAnimal duck = new Duck();
+		
+		Assert.assertEquals("Quack, Quack", duck.sing());
+		Assert.assertEquals("I am walking", duck.walk());
+	}
+	
+	@Test
+	public void testChicken() {
+		LandAnimal chicken = new Chicken(false);
+		
+		Assert.assertEquals("Cluck, Cluck", chicken.sing());
+		Assert.assertEquals("I am walking", chicken.walk());
+	}
+
+	@Test
+	public void testChickenFlying() {
+		FlyingAnimal chicken = new Chicken(false);
+		
+		Assert.assertEquals("Wings clipped", chicken.fly());
+	}
+	
+	@Test
+	public void testRooster() {
+		Rooster rooster = new Rooster();
+		
+		Assert.assertEquals("Cock-a-doodle-do", rooster.sing());
+	}
+	
+	@Test
+	public void testParrots() {
+		Parrot parrot = new Parrot(new Dog());
+		Assert.assertEquals("Woof, woof", parrot.sing());
+		
+		parrot = new Parrot(new Cat());
+		Assert.assertEquals("Meow", parrot.sing());
+		
+		parrot = new Parrot(new Rooster());
+		Assert.assertEquals("Cock-a-doodle-do", parrot.sing());
+		
+		parrot = new Parrot(new Duck());
+		Assert.assertEquals("Quack, Quack", parrot.sing());
+	}
+	
+	@Test
+	public void testShark() {
+		Fish fish = new Shark();
+		
+		Assert.assertEquals("Fish Swim",fish.swim());
+		Assert.assertEquals(COLOR.GREY,fish.getColor());
+		Assert.assertEquals(SIZE.LARGE,fish.getSize());
+		
+		Shark shark = (Shark)fish;
+		
+		Assert.assertEquals("Fish Swim",shark.swim());
+		Assert.assertEquals("Other Fish",shark.eat());
+	}
+
+	@Test
+	public void testClownFish() {
+		Fish fish = new ClownFish();
+		
+		Assert.assertEquals("Fish Swim",fish.swim());
+		Assert.assertEquals(COLOR.ORANGE,fish.getColor());
+		Assert.assertEquals(SIZE.SMALL,fish.getSize());
+		
+		ClownFish clownFish = (ClownFish)fish;
+		
+		Assert.assertEquals("Fish Swim",clownFish.swim());
+		Assert.assertEquals("joke",clownFish.joke());
+	}
+	
+	@Test
+	public void testDolphin() {
+		SwimmingAnimal dolphin = new Dolphin();
+		Assert.assertEquals("swim",dolphin.swim());
+	}
+	
+}
